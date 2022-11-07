5ce05608c726570cf6e6f93d3e49852ca8042060
MrMartinezCTC <106337782+MrMartinezCTC@users.noreply.github.com>
2022-11-07 23:03:08 +0000

    Create Switch Statement Practice

--- Program.cs
+++ Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        // Step 1: Prompt the user for a number user input comes from Console.ReadLine()
+
+        /* Step 2: Use switch statement to print the english word for the given number:
+        * 
+        * 1 --> output "Your number is one"
+        * 2 --> output "Your number is two"
+        * ...
+        * 10 --> output "Your number is ten"
+        * Use the default statement to account for any number outside of 1-10 "Your number is invalid"
+        * 
+        */
     }
 }
